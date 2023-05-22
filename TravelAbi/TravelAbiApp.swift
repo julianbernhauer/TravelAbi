750f0dfe69b180c8f3a24146c9dc27c24c7dfeb8
Julian Bernhauer <77001995+julianbernhauer@users.noreply.github.com>
2023-05-22 16:58:53 +0200

    Initial Commit

---
+++ TravelAbi/TravelAbiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAbiApp.swift
+//  TravelAbi
+//
+//  Created by Julian Bernhauer on 22.05.23.
+//
+
+import SwiftUI
+
+@main
+struct TravelAbiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
